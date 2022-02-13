3da17d28628f3de6c0c36db4dc7d1fcd0f0c1dc3
Nathan G <Nathanglass26@gmail.com>
2022-02-13 12:05:42 +0000

    theme provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
-import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { ThemeProvider } from 'styled-components';
+import type { AppProps } from 'next/app';
+import defaultTheme from 'styles/Theme';
+import Globals from 'styles/Global';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <AppHead />
-      <Component {...pageProps} />
+      <ThemeProvider theme={defaultTheme}>
+        <Globals />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   );
 }
