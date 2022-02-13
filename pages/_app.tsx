0d0ab51c443ea729a71988c6dcf2519e4959e315
Nathan G <Nathanglass26@gmail.com>
2022-02-13 11:55:36 +0000

    import fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,29 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <AppHead />
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+// seperate head tag for cleaner app
+const AppHead = () => (
+  <Head>
+    <>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@200&display=swap"
+        rel="stylesheet"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Playfair+Display&family=Work+Sans:wght@200&display=swap"
+        rel="stylesheet"
+      />
+    </>
+  </Head>
+);
 
 export default MyApp;
